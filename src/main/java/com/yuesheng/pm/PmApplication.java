517c24aa13e94a772c97c5b09d6321386f6598c6
963398090@qq.com <yueshengkeji@189.cn>
2024-02-22 09:50:21 +0800

    初始化

---
+++ src/main/java/com/yuesheng/pm/PmApplication.java
@@ -0,0 +1,13 @@
+package com.yuesheng.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmApplication.class, args);
+    }
+
+}
